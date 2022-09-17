fc524b68a36702a647daffc020ed16af98e28aaf
Flashield <zhuangbin@gmail.com>
2022-09-17 05:56:37 +0000

    Test for All Colab in codespaces.

---
+++ Test.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def hello():
+    print("Hello")
+
+def main():
+    hello()
+
+if __name__ == "__main__":
+    main()
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log("Hello, JS");
+var var1 = 1;
+console.log(var1)

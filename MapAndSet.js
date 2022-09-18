f3208a3875372c7cd9b08cb7882e56787860d135
Flashield <zhuangbin@gmail.com>
2022-09-18 06:32:54 +0000

    Javascript MapAndSet 20220918

--- MapAndSet.js
+++ MapAndSet.js
@@ -31,7 +31,123 @@ console.log(`map size:${map.size}`);
 let recipeMap = new Map([
    ['cucumber', 500],
    ['tomato', 350],
-   ['onion', 50]
+   ['onion', 50],
+   [1, 'Good'],
 ]);
 console.log(recipeMap.keys());
 console.log(map.keys());
+
+// 遍历所有键值
+for (let vegetable of recipeMap.keys()) {
+   console.log(vegetable);
+};
+// 遍历所有的值
+for (let amount of recipeMap.values()) {
+   console.log(amount);
+};
+
+// 遍历所有实体
+for (let entry of recipeMap) {
+   console.log(entry);
+};
+
+// forEach方法
+recipeMap.forEach((value, key, map) => {
+   console.log(`${key}: ${value}`)
+});
+
+
+// Object.entries：从对象创建 Map
+
+let obj = {
+   name: "John2",
+   age: 30,
+};
+
+let map1 = new Map(Object.entries(obj));
+
+console.log(map1);
+
+// Object.fromEntries：从 Map 创建对象
+obj2 = Object.fromEntries(recipeMap);
+obj3 = Object.fromEntries(recipeMap.entries());
+console.log(obj2);
+console.log(obj3);
+
+let objPrices = Object.fromEntries([
+   ['banana', 1],
+   ['orange', 2],
+   ['meat', 3],
+]);
+console.log(objPrices)
+
+// Set
+
+let set = new Set();
+
+let pete = {name: "Pete Bee"};
+let mary = {name: "Mary May"};
+
+set.add(john);
+set.add(pete);
+set.add(john);
+set.add(mary);
+
+console.log(set.size);
+
+set.forEach((value, set) => {
+   console.log(value);
+   // console.log(value2);
+});
+
+for (let value of set.values()) {
+   console.log(value);
+};
+
+for (let user of set) {
+   console.log(user.name);
+}
+
+// Set 迭代
+set.forEach((value, value2, set) => {
+   console.log(value);
+   // console.log(value2);
+});
+
+// Practice
+
+// 1
+function unique(arr){
+   return Array.from(new Set(arr));
+};
+
+let values = [1,2,3,4,5,2,4,2,4,1,];
+let uniSet = new Set(values);
+// uniSet.fromEntries(values);
+// console.log(uniSet.values());
+console.log(unique(values));
+
+// 2
+
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+function aclean(arr){
+
+   let wordMap = new Map();
+   for (let word of arr) {
+      let sorted = word.toLowerCase().split("").sort().join("");
+      wordMap.set(sorted, word);
+   };
+
+   return Array.from(wordMap.values());
+};
+
+console.log( aclean(arr) ); // "nap,teachers,ear" or "PAN,cheaters,era"
+
+
+// 3
+let map3 = new Map();
+map3.set("name", "John");
+let key3s = Array.from(map3.keys());
+key3s.push("more");
+console.log(key3s)
--- test.js
+++
@@ -1,3 +0,0 @@
-console.log("Hello, JS");
-var var1 = 1;
-console.log(var1)
